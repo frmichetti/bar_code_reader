90a3cc36ec61f64434ff804cb009b6f7c3748db6
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-05-31 11:43:40 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import axios from 'axios';
+
+(async () => {
+
+  try {
+    const response = await axios.get(`https://api.github.com/users/${process.env.GITHUB_USER}`)
+    console.log(response.data)
+  } catch (error) {
+    console.error(error)
+  }
+
+})()
